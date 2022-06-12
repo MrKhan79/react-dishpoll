67174af1c4e8ae2a01dd4c23c5df47310f40419c
Sahil Khan <sahilkhan797491@gmail.com>
2022-06-12 06:46:43 +0530

    make it better

--- src/reducers/middleware.js
+++ src/reducers/middleware.js
@@ -1,12 +1,10 @@
 import loggedReducer from "./isLogged";
 import {combineReducers} from 'redux';
 import currentUserReducer from './currentUserR'
-import dishesReducer from "./dishes";
 import dishesRank from "./dishesRankR";
 
 const allReducers = combineReducers({
     isLogged: loggedReducer,
-    dishes: dishesReducer,
     currentUser: currentUserReducer,
     dishRank: dishesRank,
 })
